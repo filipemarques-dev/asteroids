149cb00fc0dc5632bfe8e5031a21d991b92e0e72
xSoul0 <filipemarques1995@gmail.com>
2025-02-12 17:39:32 +0000

    added movement and rotation functionality for the player 'ship'

--- main.py
+++ main.py
@@ -3,21 +3,34 @@ from constants import *
 from player import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        player.draw(screen)
+            
+        updatable.update(dt)  
+      
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+        
+        #limits the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
